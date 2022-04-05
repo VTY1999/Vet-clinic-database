a1efec307aaa536c873011ad1db77c89038a1bbe
verissimo francis <verissimotoyin50@gmail.com>
2022-04-05 14:28:36 +0100

    Create new tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -10,4 +10,24 @@ CREATE TABLE animals (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE owners (
+    owners_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    full_name VARCHAR (100) NOT NULL,
+    age INT,
+    PRIMARY KEY(owners_id)
+);
+
+CREATE TABLE species (
+    species_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    PRIMARY KEY(species_id)
+);
+
 ALTER TABLE animals ADD COLUMN species varchar(100);
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD species_id INT, 
+ADD owners_id INT,
+ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(species_id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_owners FOREIGN KEY (owners_id) REFERENCES owners (owners_id) ON DELETE CASCADE
